583ea49a00b3a9d9b4c3fee0839fd8b08137e265
Chloe Steffens <caforonda103@yahoo.com>
2020-06-02 20:57:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,9 +31,12 @@ class Student
   
   def save
     sql = <<-SQL
-      
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
     
     SQL
+    
+    DB[:conn].execute(sql)
   end
   
 end
